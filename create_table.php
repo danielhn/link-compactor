af548df64ba68d63e3d4b7d57e1f129abe1b2473
danielhn <git-commits@danielhn.com>
2025-12-07 18:17:22 +0100

    Add file to create table in database

---
+++ create_table.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/env.php';
+require_once __DIR__ . '/config.php';
+
+$database = new PDO(SQLITE_DATABASE_PATH);
+
+$sql = "CREATE TABLE IF NOT EXISTS linkshortener 
+        (id VARCHAR(" . SHORT_LINK_ID_LENGTH . ") PRIMARY KEY,
+         url VARCHAR(" . URL_MAX_LENGTH . ") NOT NULL,
+         created_at DATE NOT NULL,
+         visits INT NOT NULL)";
+
+$database->query($sql);
