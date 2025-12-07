d32568789f71fb4de2d2538dc48a84bb959fece3
danielhn <git-commits@danielhn.com>
2025-12-07 11:55:32 +0100

    Add basic index page

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link shortener</title>
+</head>
+<body>
+    <main>
+        <h1>Link Shortener</h1>
+        <form action="" method="post">
+            <label for="url">Link to shorten</label>
+            <input value="" type="url" name="url" id="url">
+            <button type="submit">Create short link</button>
+        </form>
+    </main>
+</body>
+</html>
