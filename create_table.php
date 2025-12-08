8715de4fbb9685157822d6232ed39d6cef6c49ad
danielhn <git-commits@danielhn.com>
2025-12-08 11:38:38 +0100

    Add not null to id and unique to url

--- create_table.php
+++ create_table.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/config.php';
 $database = new PDO(SQLITE_DATABASE_PATH);
 
 $sql = "CREATE TABLE IF NOT EXISTS linkshortener 
-        (id VARCHAR(" . SHORT_LINK_ID_LENGTH . ") PRIMARY KEY,
-         url VARCHAR(" . URL_MAX_LENGTH . ") NOT NULL)";
+        (id VARCHAR(" . SHORT_LINK_ID_LENGTH . ") PRIMARY KEY NOT NULL,
+         url VARCHAR(" . URL_MAX_LENGTH . ") UNIQUE NOT NULL)";
 
 $database->query($sql);
