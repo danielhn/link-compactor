79cf3a0766f7b31f6f4d4ac0e5449ebe4dc8e3a7
danielhn <git-commits@danielhn.com>
2025-12-08 17:32:56 +0100

    Show the full original link when going to a short link

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 require_once __DIR__ .  '/config.php';
+require_once __DIR__ . '/env.php';
+
+if ($_SERVER['REQUEST_URI'] !== '/index.php' && $_SERVER['REQUEST_URI'] !== '/') {
+    $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+
+    // First string in path should be short link id
+    $shortLinkId = $requestURI[1];
+    if (validateShortLinkId($shortLinkId)) {
+        $db = new PDO(SQLITE_DATABASE_PATH);
+
+        $sql = "SELECT url FROM linkshortener WHERE id = ? LIMIT 1";
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$shortLinkId]);
+        $originalLink = $stmt->fetch(PDO::FETCH_ASSOC);
+        $url = $originalLink['url'];
+    }
+
+}
+
+function validateShortLinkId(string $shortLinkId): false|int
+{
+    return preg_match("/^[a-zA-Z0-9]{" . SHORT_LINK_ID_LENGTH . "}$/", $shortLinkId);
+}
+
 ?>
 
 <!doctype html>
@@ -11,12 +36,20 @@ require_once __DIR__ .  '/config.php';
 </head>
 <body>
     <main>
+        <?php if (empty($url)): ?>
         <h1>Link Shortener</h1>
         <form action="./shorten_link.php" method="post">
             <label for="url">Link to shorten</label>
             <input type="url" name="url" id="url" maxlength="<?= URL_MAX_LENGTH ?>" required>
             <button type="submit">Create short link</button>
         </form>
+        <?php else: ?>
+        <h1>Check the link first</h1>
+            <p>You are going to <a rel="noopener noreferer nofollow" href="<?= $url ?>"><?= $url ?></a>.
+                If you want to continue, click on the link.
+            </p>
+        <p>Or you can <a href="/">go back to the homepage</a>.</p>
+        <?php endif ?>
     </main>
 </body>
 </html>
