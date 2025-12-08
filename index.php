3dc665141178167c0caf121c023e5303e88ed845
danielhn <git-commits@danielhn.com>
2025-12-08 18:42:17 +0100

    Add page not found section

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ if ($_SERVER['REQUEST_URI'] !== '/index.php' && $_SERVER['REQUEST_URI'] !== '/')
         $stmt->execute([$shortLinkId]);
         $originalLink = $stmt->fetch(PDO::FETCH_ASSOC);
         $url = $originalLink['url'];
+    } else {
+        $pageNotFound = true;
     }
 
 }
@@ -36,19 +38,23 @@ function validateShortLinkId(string $shortLinkId): false|int
 </head>
 <body>
     <main>
-        <?php if (empty($url)): ?>
+        <?php if (empty($url) && empty($pageNotFound)): ?>
         <h1>Link Shortener</h1>
         <form action="./shorten_link.php" method="post">
             <label for="url">Link to shorten</label>
             <input type="url" name="url" id="url" maxlength="<?= URL_MAX_LENGTH ?>" required>
             <button type="submit">Create short link</button>
         </form>
-        <?php else: ?>
+        <?php elseif (!empty($url)): ?>
         <h1>Check the link first</h1>
             <p>You are going to <a rel="noopener noreferer nofollow" href="<?= $url ?>"><?= $url ?></a>.
                 If you want to continue, click on the link.
             </p>
         <p>Or you can <a href="/">go back to the homepage</a>.</p>
+        <?php elseif (!empty($pageNotFound)): ?>
+            <?php http_response_code(404) ?>
+            <h1>The page you tried to go doesn't exist</h1>
+            <p><a href="/">Go back to the homepage</a>.</p>
         <?php endif ?>
     </main>
 </body>
