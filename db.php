7bef2195bb96076d1eb8a8e712fcd4b6df02f358
danielhn <git-commits@danielhn.com>
2026-05-08 16:24:56 +0200

    Throw exception if database type is not recognized

--- db.php
+++ db.php
@@ -15,11 +15,13 @@ if (DATABASE_TYPE === 'sqlite') {
         ";dbname=" . MYSQL_DATABASE_NAME .
         ";charset=". MYSQL_DATABASE_CHARSET;
     $db = new PDO($dsn, MYSQL_DATABASE_USER, MYSQL_DATABASE_PASSWORD, $options);
-} elseif (DATABASE_TYPE === 'pgsql') {
+} else if (DATABASE_TYPE === 'pgsql') {
     $dsn = "pgsql:host=" . PGSQL_DATABASE_HOST .
         ";port=" . PGSQL_DATABASE_PORT .
         ";dbname=" . PGSQL_DATABASE_NAME;
     $db = new PDO($dsn, PGSQL_DATABASE_USER, PGSQL_DATABASE_PASSWORD, $options);
+} else {
+    throw new Exception('Unrecognized database type. Available types: sqlite, mysql, pgsql.');
 }
 
 return $db;
